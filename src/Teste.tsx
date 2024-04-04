833c695a154d4d986bbbb2b57a63c43c735d251c
pedroDev <playstarpedro@gmail.com>
2024-04-04 19:08:42 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/Teste.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+  fontSize?: string
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+  font-size: ${(props) => props.fontSize || '16px'};
+`
+const BotaoPerigo = styled(Botao)`
+  background-color: red;
+  color: #fff;
+
+  span {
+    text-decoration: line-through;
+  }
+`
+
+function Teste() {
+  return (
+    <>
+      <Botao fontSize="18px" principal>
+        Clique aqui
+      </Botao>
+      <Botao fontSize="14px" principal={false}>
+        Clique aqui
+      </Botao>
+      <BotaoPerigo as="a" principal>
+        <span>Não clique aqui</span>
+      </BotaoPerigo>
+    </>
+  )
+}
+
+export default Teste
